66102283e585266559eccd909cb3fa8d63043b34
bnw <s6beklot@uni-bonn.de>
2017-06-10 14:10:38 +0200

    Refactored reader

--- src/Reader.h
+++ src/Reader.h
@@ -11,34 +11,7 @@
 class Reader
 {
 public:
-	std::vector<Terminal> read(std::istream &input) const
-	{
-		std::vector<Terminal> terminals;
-		size_t n;
-		input >> n;
-		for (size_t i = 0; i < n; i++) {
-			input >> std::ws;
-			if (input.eof()) {
-				throw std::runtime_error("Input contains less than " + std::to_string(n) + " data rows.");
-			}
-			std::string line_buffer;
-			std::getline(input, line_buffer);
-			std::istringstream line_stream(line_buffer);
-
-			Coord x, y, z;
-			line_stream >> x >> y >> z >> std::ws;
-			terminals.emplace_back(Position{x, y, z}, i);
-			if (not line_stream.eof()) {
-				throw std::runtime_error("Data line contains more than 3 numbers.");
-			}
-		}
-		input >> std::ws;
-		if (not input.eof()) {
-			throw std::runtime_error("Input contains more than " + std::to_string(n) + " data rows.");
-		}
-		return terminals;
-	}
-
+	std::vector<Terminal> read(std::istream &input) const;
 };
 
 
