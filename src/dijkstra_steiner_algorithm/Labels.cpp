77827e17b0491f0fb714bbf39a922bce009b589b
bnw <s6beklot@uni-bonn.de>
2017-06-10 14:13:26 +0200

    Decrease memory usage of Labels

--- src/dijkstra_steiner_algorithm/Labels.cpp
+++ src/dijkstra_steiner_algorithm/Labels.cpp
@@ -5,45 +5,40 @@ namespace dijkstra_steiner_algorithm {
 
 Labels::Labels(Instance const &instance) :
 		graph(instance.graph),
-		labels(graph.num_nodes() * pow2(instance.terminals.size()), std::numeric_limits<Coord>::max()),
-		permanently_labeled(graph.num_nodes() * pow2(instance.terminals.size()), false),
-		permanently_labeled_subsets(graph.num_nodes())
+		labels(graph.num_nodes()),
+		permanently_labeled(graph.num_nodes())
 {}
 
 Coord Labels::get(NodePlusTerminalSubset const &node_plus_terminal_subset) const
 {
-	return labels.at(get_index(node_plus_terminal_subset));
+	if (labels.at(graph.get_index(node_plus_terminal_subset.node)).count(node_plus_terminal_subset.subset)) {
+		return labels.at(graph.get_index(node_plus_terminal_subset.node)).at(node_plus_terminal_subset.subset);
+	} else {
+		return std::numeric_limits<Coord>::max();
+	}
 }
 
 void Labels::set(NodePlusTerminalSubset const &node_plus_terminal_subset, Coord const new_value)
 {
-	labels.at(get_index(node_plus_terminal_subset)) = new_value;
-}
-
-size_t Labels::get_index(NodePlusTerminalSubset const &node_plus_terminal_subset) const
-{
-	return
-			node_plus_terminal_subset.subset.get_index()
-			+ graph.get_index(node_plus_terminal_subset.node) * pow2(node_plus_terminal_subset.subset.max_size());
+	labels.at(graph.get_index(node_plus_terminal_subset.node))[node_plus_terminal_subset.subset] = new_value;
 }
 
 void Labels::mark_permanently_labeled(NodePlusTerminalSubset const &node_plus_terminal_subset)
 {
 	assert(not is_permanently_labeled(node_plus_terminal_subset));
-	permanently_labeled[get_index(node_plus_terminal_subset)] = true;
-	permanently_labeled_subsets
-			.at(graph.get_index(node_plus_terminal_subset.node))
-			.push_back(node_plus_terminal_subset.subset);
+	permanently_labeled.at(graph.get_index(node_plus_terminal_subset.node)).insert(node_plus_terminal_subset.subset);
 }
 
 bool Labels::is_permanently_labeled(NodePlusTerminalSubset const &node_plus_terminal_subset) const
 {
-	return permanently_labeled[get_index(node_plus_terminal_subset)];
+	return 0 != (permanently_labeled
+			.at(graph.get_index(node_plus_terminal_subset.node))
+			.count(node_plus_terminal_subset.subset));
 }
 
-std::vector<TerminalSubset> const &Labels::get_permanently_labeled_subsets_for_node(graph::Node const &node) const
+std::set<TerminalSubset> const &Labels::get_permanently_labeled_subsets_for_node(graph::Node const &node) const
 {
-	return permanently_labeled_subsets.at(graph.get_index(node));
+	return permanently_labeled.at(graph.get_index(node));
 }
 
 }
--- src/dijkstra_steiner_algorithm/Labels.h
+++ src/dijkstra_steiner_algorithm/Labels.h
@@ -2,6 +2,7 @@
 #define CHIP_DESIGN_LABELS_H
 
 
+#include <map>
 #include "../graph/HananGridGraph.h"
 #include "../TerminalSubset.h"
 #include "NodePlusTerminalSubset.h"
@@ -10,29 +11,34 @@ namespace dijkstra_steiner_algorithm {
 
 class Instance;
 
+/**
+ * Stores the labels used in the Dijkstra Steiner Algorithm.
+ */
 class Labels
 {
 public:
 	Labels(Instance const &instance);
 
+	/**
+	 * Retrieve a label.
+	 */
 	Coord get(NodePlusTerminalSubset const &node_plus_terminal_subset) const;
 
+	/**
+	 * Create or update a label.
+	 */
 	void set(NodePlusTerminalSubset const &node_plus_terminal_subset, Coord const new_value);
 
 	void mark_permanently_labeled(NodePlusTerminalSubset const &node_plus_terminal_subset);
 
 	bool is_permanently_labeled(NodePlusTerminalSubset const &node_plus_terminal_subset) const;
 
-	std::vector<TerminalSubset> const &get_permanently_labeled_subsets_for_node(graph::Node const &node) const;
-
-	//public for unit tests
-	size_t get_index(NodePlusTerminalSubset const &node_plus_terminal_subset) const;
+	std::set<TerminalSubset> const &get_permanently_labeled_subsets_for_node(graph::Node const &node) const;
 
 private:
 	graph::HananGridGraph const &graph;
-	std::vector<Coord> labels;
-	std::vector<bool> permanently_labeled;
-	std::vector<std::vector<TerminalSubset>> permanently_labeled_subsets;
+	std::vector<std::map<TerminalSubset, Coord>> labels; // one map for every node
+	std::vector<std::set<TerminalSubset>> permanently_labeled; // one set for every node
 };
 
 }
