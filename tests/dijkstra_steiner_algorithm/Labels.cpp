e6153d9d7dbc1d370e87c48aa119a41cbb7a9f03
bnw <s6beklot@uni-bonn.de>
2017-06-10 14:13:45 +0200

    Added tests for Labels

--- tests/dijkstra_steiner_algorithm/Labels.cpp
+++ tests/dijkstra_steiner_algorithm/Labels.cpp
@@ -2,10 +2,20 @@
 #include "../../src/dijkstra_steiner_algorithm/Labels.h"
 #include "../../src/dijkstra_steiner_algorithm/DijkstraSteinerAlgorithm.h"
 
+using namespace dijkstra_steiner_algorithm;
 
-BOOST_AUTO_TEST_CASE(TestLabels_get_index)
+struct LabelsFixture
 {
-	using namespace dijkstra_steiner_algorithm;
+	LabelsFixture()
+	{
+		for (auto const x : {0, 1, 2, 3}) {
+			for (auto const y : {0, 1, 2, 3}) {
+				for (auto const z : {0, 1, 2, 3}) {
+					nodes.push_back(graph.create_node({x, y, z}));
+				}
+			}
+		}
+	}
 
 	Terminal::Vector terminals = {
 			{{0, 0, 0}, 0},
@@ -13,31 +23,40 @@ BOOST_AUTO_TEST_CASE(TestLabels_get_index)
 			{{2, 2, 2}, 2},
 			{{3, 3, 3}, 3}
 	};
-	auto const instance = Instance::create(terminals);
-	auto const &graph = instance.graph;
-
+	Instance const instance = Instance::create(terminals);
+	Labels labels = {instance};
+	graph::HananGridGraph const &graph = instance.graph;
 	std::vector<graph::Node> nodes;
-	for (auto const x : {0, 1, 2, 3}) {
-		for (auto const y : {0, 1, 2, 3}) {
-			for (auto const z : {0, 1, 2, 3}) {
-				nodes.push_back(graph.create_node({x, y, z}));
-			}
-		}
-	}
+};
 
-	Labels const labels(instance);
+BOOST_FIXTURE_TEST_CASE(TestLabels_permanently_labeled, LabelsFixture)
+{
 	auto const full_subset = TerminalSubset::create_full(terminals);
+	auto const node1 = nodes.front();
+	auto const node2 = nodes.back();
 
-	std::set<size_t> seen_indices;
-	size_t max_index = 0;
-	for (auto const &subset : full_subset.create_nonempty_subsets()) {
-		for (auto const &node : nodes) {
-			auto const index = labels.get_index({node, subset});
-			BOOST_TEST(0 == seen_indices.count(index));
-			seen_indices.insert(index);
-			max_index = std::max(max_index, index);
-		}
-	}
-	BOOST_TEST(nodes.size() * (pow2(terminals.size()) - 1) == seen_indices.size());
-	BOOST_TEST(max_index <= seen_indices.size() + nodes.size());
+	BOOST_TEST(not labels.is_permanently_labeled({node1, full_subset}));
+	BOOST_TEST(not labels.is_permanently_labeled({node2, full_subset}));
+	labels.mark_permanently_labeled({node1, full_subset});
+	BOOST_TEST(labels.is_permanently_labeled({node1, full_subset}));
+	BOOST_TEST(not labels.is_permanently_labeled({node2, full_subset}));
+	BOOST_TEST(std::set<TerminalSubset>{full_subset} == labels.get_permanently_labeled_subsets_for_node(node1));
+	BOOST_TEST(std::set<TerminalSubset>{} == labels.get_permanently_labeled_subsets_for_node(node2));
+	labels.mark_permanently_labeled({node2, full_subset});
+	BOOST_TEST(labels.is_permanently_labeled({node1, full_subset}));
+	BOOST_TEST(labels.is_permanently_labeled({node2, full_subset}));
+}
+
+BOOST_FIXTURE_TEST_CASE(TestLabels_get_set, LabelsFixture)
+{
+	auto const full_subset = TerminalSubset::create_full(terminals);
+	auto const node1 = nodes.front();
+	auto const node2 = nodes.back();
+
+	BOOST_TEST(100000 < labels.get({node1, full_subset}));
+	BOOST_TEST(100000 < labels.get({node2, full_subset}));
+	labels.set({node1, full_subset}, 1);
+	labels.set({node2, full_subset}, 2);
+	BOOST_TEST(1 == labels.get({node1, full_subset}));
+	BOOST_TEST(2 == labels.get({node2, full_subset}));
 }
